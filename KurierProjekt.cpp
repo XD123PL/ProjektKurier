36dad28bfc6477740469a00f6f2522ea6bae2ded
XD123PL <92270149+XD123PL@users.noreply.github.com>
2021-12-30 23:26:42 +0100

    Początek projektu
    Wczytanie danych (do zmiany, żeby brało z gitbasha nazwe pliku) + pobieranie tylko cyfr z tesktu (Numery klientów i odległości)

---
+++ KurierProjekt.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <string.h>
+#include <vector>
+int main(int argc, char* argv[]) 
+{
+    //string nazwaPliku = argv[0]; NIE WIEM CZY DOBRZE
+    std::fstream plik;
+    plik.open("test.txt"); //, std::fstream::in | std::fstream::out | std::fstream::app);
+    char o;
+    std::vector<std::string>K; //Para klientów
+    std::vector<int>Odl; //Odległosc między klientami: K[1] = Odl[1], K[2] = Odl[2], K[3] = Odl[3], K[4]...
+    char a, b; // np Klient1 -> Klient2
+    while (plik >> o)
+    {
+        if (o >= 48 && o <= 58)
+        {
+            a = o;
+            while (plik >> o)
+            {
+                if (o >= 48 && o <= 58)
+                {
+                    b = o;
+                    while (plik >> o)
+                    {
+                        if (o >= 48 && o <= 58)
+                            Odl.push_back(o);
+                    }
+                }
+            }
+        }
+    }
+    /*std::string inFile, outFile;
+    inFile = argv[0];
+    std::cout << inFile;*/
+    
+    std::string a1 = "a", b1 = "b";
+    //int c = a + b;
+    std::string test = a1 + b1;
+    std::cout << test;
+    std::cout << "Hello World!\n";
+}
